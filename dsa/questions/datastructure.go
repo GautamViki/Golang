f8d30ccf9137dad10a91ca0ee157dea2335e3717
Vikas Kumar Gautam <vikacse@gmail.com>
2024-07-19 01:26:34 +0530

    Add binary

--- dsa/questions/datastructure.go
+++ dsa/questions/datastructure.go
@@ -198,3 +198,17 @@ func (ds *DataStructure) MajorityElement() {
 	}
 	fmt.Println("MajorityElement", ans)
 }
+
+func (ds *DataStructure) AddBinary() {
+	num1 := "101101"
+	num2 := "100011"
+	i := len(num1) - 1
+	j := len(num2) - 1
+	carry := 0
+	result := ""
+	for i >= 0 || j >= 0 {
+		if num1[i] == num2[j] {
+
+		}
+	}
+}
