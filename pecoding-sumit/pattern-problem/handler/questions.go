1aa0fbdd71ea74a4283234ad99eb820110836a6d
Vikas Kumar Gautam <vikacse@gmail.com>
2024-08-07 02:28:41 +0530

    pepcoding by sumit

--- pecoding-sumit/pattern-problem/handler/questions.go
+++ pecoding-sumit/pattern-problem/handler/questions.go
@@ -9,7 +9,7 @@ func NewPatternQuestions() *pattern {
 }
 
 func (p *pattern) Print_Z() {
-	num := 10
+	num := 3
 	for i := 1; i <= num; i++ {
 		if i == 1 || i == num {
 			for i := 0; i < num; i++ {
@@ -23,4 +23,15 @@ func (p *pattern) Print_Z() {
 		}
 		fmt.Println()
 	}
+}
+
+func (p *pattern) IsNumPrime(num int) {
+	isPrime := true
+	for i := 2; i*i < num; i++ {
+		if num%i == 0 {
+			isPrime = false
+			break
+		}
+	}
+	fmt.Println("Is this prime number : ", isPrime)
 }
--- pecoding-sumit/pattern-problem/main.go
+++ pecoding-sumit/pattern-problem/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"pattern-problem/handler"
-
 )
 
 func main() {
 	fmt.Println("pattern")
 	pattern := handler.NewPatternQuestions()
 	pattern.Print_Z()
+	pattern.IsNumPrime(11)
 }
