eea49262d33dccf98558c416e240c660153da6e9
Vikas Kumar Gautam <vikacse@gmail.com>
2024-08-21 15:11:13 +0530

    recursion

---
+++ pecoding-sumit/recursion-and-backtracking/handler/recursion.go
@@ -0,0 +1,29 @@
+package handler
+
+func NewRecursion() *recursion {
+	return &recursion{}
+}
+
+type recursion struct{}
+
+func (re *recursion) FindFirstOccurrence(nums []int, idx, target int) int {
+	// nums := []int{2, 4, 3, 8, 5, 2, 3, 5, 9, 6}
+	if idx == len(nums)-1 {
+		return -1
+	}
+	if nums[idx] == target {
+		return idx
+	}
+	return re.FindFirstOccurrence(nums, idx+1, target)
+}
+
+func (re *recursion) FindLastOccurrence(nums []int, idx, target, ans int) int {
+	// nums := []int{2, 4, 3, 8, 5, 2, 3, 5, 9, 6}
+	if nums[idx] == target {
+		ans = idx
+	}
+	if idx == len(nums)-1 {
+		return ans
+	}
+	return re.FindLastOccurrence(nums, idx+1, target, ans)
+}
---
+++ pecoding-sumit/recursion-and-backtracking/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"recursion-backtracking/handler"
+)
+
+func main() {
+	recursion := handler.NewRecursion()
+	nums := []int{2, 4, 3, 8, 5, 2, 3, 5, 9, 6}
+	firstOccurrence := recursion.FindFirstOccurrence(nums, 0, 30)
+	fmt.Println("First Occurrence of a number : ", firstOccurrence)
+	lastOccurrence := recursion.FindLastOccurrence(nums, 0, 61, -1)
+	fmt.Println("Last Occurrence of a number : ", lastOccurrence)
+}
